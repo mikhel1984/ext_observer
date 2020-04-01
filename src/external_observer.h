47cdd41ed13e8cb8cd6b9b1c51160a83174d663b
Stas Mikhel <mikhel.sk@gmail.com>
2020-04-01 19:08:47 +0300

    Split interface and realization for rnea class

--- src/external_observer.h
+++ src/external_observer.h
@@ -80,15 +80,7 @@ public:
   /**
    * @brief Object constructor.
    */
-  RobotDynamicsRnea()
-  : RobotDynamicsBase()
-  , _qext(Vector(1))
-  , _p0(Vector(1))
-  , _zero(Vector(1))
-  , _sum(Vector(1))
-  , _M(Matrix(1,1))
-  , delta(1E-7) 
-  {   }
+  RobotDynamicsRnea();  
   /** 
    * @brief Call recursibe Newton-Euler algorithm.
    * @param q joint angle vector.
@@ -206,44 +198,6 @@ protected:
 
 }; // ExternalObserverRnea
 
-// Find C^T * qd
-Vector RobotDynamicsRnea::tranCqd(Vector& q, Vector& qd)
-{
-  // TODO: call it once
-  int N = jointNo();
-  _zero.resize(N); _zero.setZero();
-  _p0 = rnea(q,_zero,qd);  // M * qd
-  _sum.setZero();
-
-  for(int i = 0; i < N; i++) {
-    _qext = q;
-    _qext(i) += delta; 
-    _sum += (rnea(_qext,_zero,qd) - _p0) * (qd(i) / delta);
-  }
-  _sum -= rnea(q,qd,_zero); // M'*qd - C*qd
-
-  return _sum;
-}
-
-// Create M matrix from sequence of RNEA calls
-Matrix RobotDynamicsRnea::getM(Vector& q)
-{
-  // TODO: call it once
-  int N = jointNo();
-  _zero.resize(N); _zero.setZero();
-  _M.resize(N,N); 
-  _qext.resize(N);
-  
-  for(int i = 0; i < N; i++) {
-    _qext.setZero();  // use _qext to choose column
-    _qext(i) = 1;   
-    _p0 = rnea(q,_zero,_qext); // use _p0 to save matrix column
-    for(int j = 0; j < N; j++)
-      _M(j,i) = _p0(j);
-  }
-  
-  return _M;
-}
 
 #endif // EXTERNAL_OBSERVER_H
 
