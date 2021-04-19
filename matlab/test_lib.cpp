1b53dd25a16ee07c3d540f0a633d5a4771aa1415
Stanislav <mikhel.sk@gmail.com>
2021-04-19 17:17:03 +0300

    Fix indexation

--- matlab/test_lib.cpp
+++ matlab/test_lib.cpp
@@ -17,29 +17,28 @@
 #define BEG_Q    1
 #define BEG_QD   (BEG_Q+JOINT_NO)
 #define BEG_TAU  (BEG_QD+JOINT_NO)
-#define BEG_Q2D  (BEG_TAU+JOINT_NO)
 
 // Reag csv line of numbers
 bool csvRead(std::istream& str, std::vector<double>& v);
 
 
 int main(int argc, char** argv)
-{  
-  int N = JOINT_NO;
+{   
   double q[JOINT_NO], qd[JOINT_NO], tau[JOINT_NO], ext[JOINT_NO];
 
   // torque coefficients
-  double K[] = {10.0,10.6956};
+  double K[] = {1,1};      // current to force, N/A
 
+  // test with Kalman disturbance observer
   double S[JOINT_NO*JOINT_NO] = {0};
   double H[] = {1,0,
                 0,1};   
   double Q[(2*JOINT_NO)*(2*JOINT_NO)] = {0};
-  for(int i = 0; i < (2*JOINT_NO)*(2*JOINT_NO); i += JOINT_NO+1) {
+  for(int i = 0; i < (2*JOINT_NO)*(2*JOINT_NO); i += 2*JOINT_NO+1) {
     Q[i] = (i < 2*JOINT_NO*JOINT_NO) ? 0.2 : 30;    
   }
   double R[] = {5E-4,0,
-                0,5E-4};
+                   0,5E-4};
   int id_kfe = configDistKalmanObserverExp(-1,S,H,Q,R);
   
   
@@ -52,16 +51,14 @@ int main(int argc, char** argv)
   std::ifstream iFile(IN_NAME);
 
   std::vector<double> val;
-  val.reserve(JOINT_NO);
+  val.reserve(3*JOINT_NO+1);
 
   double prev = -1, zero = 0, dt = 0;
-  // range
-  //double tBeg = -1, tEnd = 1E100;
-  
+    
   while(csvRead(iFile,val)) {
 
     double curr = val[0];
-    if(prev > 0) {
+    if(prev >= 0) {
       dt = curr - prev;
     } else {
       dt = 0;
@@ -70,14 +67,11 @@ int main(int argc, char** argv)
     prev = curr;
     curr -= zero;  // start from 0
 
-    //if(curr < tBeg || curr > tEnd) continue;
-
     // read values
     for(int i = 0; i < JOINT_NO; i++) {
       q[i]   = val[BEG_Q + i];
       qd[i]  = val[BEG_QD + i];
-      tau[i] = val[BEG_TAU + i] * K[i];
-      //q2d(i) = val[BEG_Q2D + i];
+      tau[i] = val[BEG_TAU + i] * K[i];      
     }
     
     getExternalTorque(id_kfe,ext,q,qd,tau,dt);
